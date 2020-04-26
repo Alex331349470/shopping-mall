09d48b92fe2b1c0c35a2384b07d98fe9e702a810
vagrant <vagrant@homestead>
2020-04-26 13:35:50 +0000

    pay init & queue init

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\UserAddress;
+use App\Observers\UserAddressObserver;
 use Illuminate\Http\Resources\Json\Resource;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +18,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            $config['notify_url'] = env('APP_URL').'/payment/wechat/notify';
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个微信支付对象
+            return Pay::wechat($config);
+        });
     }
 
     /**
@@ -25,5 +39,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Resource::withoutWrapping();
+        UserAddress::observe(UserAddressObserver::class);
     }
 }
