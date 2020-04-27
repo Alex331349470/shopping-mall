e592253ca9bf60955543fc6a69d689c7507a6a32
vagrant <vagrant@homestead>
2020-04-27 13:40:35 +0000

    wechat mini app pay

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -15,18 +15,14 @@ class PaymentsController extends Controller
             abort(403, '订单状态不正确');
         }
 
-        $wechatOrder = app('wechat_pay')->scan([
+        return app('wechat_pay')->miniapp([
             'out_trade_no' => $order->no,
             'total_fee' => $order->total_amount * 100,
             'body' => '支付订单:' . $order->no,
         ]);
-
-        $qrCode = new QrCode($wechatOrder->code_url);
-
-        return response($qrCode->writeDataUri(), 200);
     }
 
-    public function payByAlipay(Order $order,Request $request)
+    public function payByAlipay(Order $order, Request $request)
     {
         if ($order->paid_at || $order->closed) {
             abort(403, '订单状态不正确');
@@ -35,7 +31,7 @@ class PaymentsController extends Controller
         return app('alipay')->web([
             'out_trade_no' => $order->no,
             'total_amount' => $order->total_amount,
-            'subject' => '支付订单：'.$order->no,
+            'subject' => '支付订单：' . $order->no,
         ]);
     }
 }
