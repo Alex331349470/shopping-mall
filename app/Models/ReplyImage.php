3c4cab59f16c84ddff43ef03fe9e6f6fde048693
vagrant <vagrant@homestead>
2020-05-02 03:21:50 +0000

    add replyimage model

---
+++ app/Models/ReplyImage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReplyImage extends Model
+{
+    protected $fillable = ['good_id','user_id','order_id','path'];
+
+
+}
