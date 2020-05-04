07dcff05fc5d680c67029e8abc27331b5fe81071
vagrant <vagrant@homestead>
2020-05-04 14:04:33 +0000

    bonus total

--- app/Http/Controllers/Api/BonusesController.php
+++ app/Http/Controllers/Api/BonusesController.php
@@ -18,6 +18,12 @@ class BonusesController extends Controller
     {
         $total = \DB::table('bonuses')->where('user_id',$request->user()->id)->sum('bonus');
 
+        if (!$total)  {
+            return response([
+                'total' => 0,
+            ])->setStatusCode(200);
+        }
+
         return response([
             'total' => $total
         ])->setStatusCode(200);
