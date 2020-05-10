11e204c1686d7ec541bc472c53dae8dd4862113c
vagrant <vagrant@homestead>
2020-05-10 16:05:05 +0000

    init sys

--- app/Admin/Extensions/OrderExcelExporter.php
+++ app/Admin/Extensions/OrderExcelExporter.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Admin\Extensions;
 
-use App\Models\Order;
 use Encore\Admin\Grid\Exporters\ExcelExporter;
 use Maatwebsite\Excel\Concerns\WithMapping;
 
@@ -37,47 +36,23 @@ class OrderExcelExporter extends ExcelExporter implements WithMapping {
      */
     public function map($order): array
     {
-        $ship_status = (Order::$shipStatusMap[$order->ship_status])??"";
-        $refund_status = (Order::$refundStatusMap[$order->refund_status])??"";
-        $ship_data = '';
-        if (isset($order->ship_data['express_company'])) {
-            $ship_data .= '物流公司：' . $order->ship_data['express_company'];
-        }
-        if (isset($order->ship_data['express_no'])) {
-            $ship_data .= ' 订单编号：' . $order->ship_data['express_no'];
-        }
-
-        $address = '';
-        if (isset($order->address['address'])) {
-            $address .= '地址：' . mb_convert_encoding($order->address['address'], 'UTF8');
-        }
-        if (isset($order->address['zip'])) {
-            $address .= ' 邮编：' . $order->address['zip'];
-        }
-        if (isset($order->address['contact_name'])) {
-            $address .= ' 联系人：' . mb_convert_encoding($order->address['contact_name'], 'UTF8');
-        }
-        if (isset($order->address['contact_phone'])) {
-            $address .= ' 电话：' . mb_convert_encoding($order->address['contact_phone'], 'UTF8');
-        }
-
         return [
             $order->id,
             $order->no,
             $order->name,
-            $address,
+            $order->address,
             $order->total_amount,
             $order->remark,
             $order->paid_at,
             $order->payment_method,
             $order->payment_no,
-            $refund_status,
+            $order->refund_status,
             $order->refund_no,
-            $order->closed == 0?'否':'是',
-            $order->reply_status == 0?'否':'是',
-            $order->cancel == 0?'否':'是',
-            $ship_status,
-            $ship_data,
+            $order->closed,
+            $order->reply_status,
+            $order->cancel,
+            $order->ship_status,
+            $order->ship_data,
             $order->extra,
             $order->created_at
         ];
