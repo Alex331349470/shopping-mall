86cdba2e3fe6beff54a7b854c3d9b38fc34d27ff
vagrant <vagrant@homestead>
2020-05-12 15:36:20 +0000

    init

--- routes/api.php
+++ routes/api.php
@@ -66,7 +66,7 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
             //获取单个商品评论图片
             Route::get('goods/{good}/reply_images'.'GoodsController@replyImageIndex')
                 ->name('replies.images.index');
-            
+
             Route::get('goods/{good}/skus','GoodsController@skus')
                 ->name('goods.skus');
             //获取单个商品评论
