1a4a0ca93656ac6ad5fdf0bc757c149cc31f600e
vagrant <vagrant@homestead>
2020-05-14 05:44:50 +0000

    init bonus

---
+++ app/Listeners/CreateSoldBonus.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPaid;
+use App\Models\Bonus;
+use http\Client\Curl\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class CreateSoldBonus
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param OrderPaid $event
+     * @return void
+     */
+    public function handle(OrderPaid $event)
+    {
+        $order = $event->getOrder();
+
+        $user = User::find($order->user_id);
+
+        if ($parent_id = $user->userInfo->parent_id) {
+
+            $total_amount = $order->total_amount;
+
+            $bonus = ($total_amount*2)/100;
+
+            $parent_user = User::find($parent_id);
+
+            Bonus::create([
+                'user_id' => $parent_id,
+                'order_id' => $order->id,
+                'user_type' => $parent_user->userInfo->type,
+                'bonus' => $bonus,
+            ]);
+        }
+
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Events\OrderPaid;
+use App\Listeners\CreateSoldBonus;
 use App\Listeners\UpdateProductSoldCount;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -22,6 +23,7 @@ class EventServiceProvider extends ServiceProvider
         ],
         OrderPaid::class => [
             UpdateProductSoldCount::class,
+            CreateSoldBonus::class,
         ],
     ];
 
