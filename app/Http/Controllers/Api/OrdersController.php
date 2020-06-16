ff54591c075263ad9a01cbe740059b26f5a8d771
vagrant <vagrant@homestead>
2020-06-16 14:34:55 +0000

    init skus

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -96,7 +96,7 @@ class OrdersController extends Controller
                     ]);
 
                     $item->good()->associate($sku->good);
-                    $item->sku_id = $sku_id;
+                    $item->sku()->associate($sku);
                     $item->save();
                     $totalAmount += $sku->price * $amount;
                     $totalWeight += $sku->good->weight * $amount;
