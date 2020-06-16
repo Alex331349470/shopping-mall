61c0b32a177a7b3d816b483ada79582a7da0661d
vagrant <vagrant@homestead>
2020-06-16 13:47:23 +0000

    init skus

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -19,7 +19,7 @@ class OrdersController extends Controller
 {
     public function index(Request $request)
     {
-        $orders = Order::where('user_id', $request->user()->id)->with('user', 'items.good.images', 'items.good.category')->orderBy('created_at', 'desc')->get();
+        $orders = Order::where('user_id', $request->user()->id)->with('user', 'items.good.images', 'items.good.category','items.sku')->orderBy('created_at', 'desc')->get();
 
 
         OrderResource::wrap('data');
