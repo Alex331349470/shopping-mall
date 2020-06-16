93d6aaa2ef26470d7d99b20b74a6696282fb187c
vagrant <vagrant@homestead>
2020-06-16 14:10:48 +0000

    init skus

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    protected $fillable = ['amount', 'price', 'rating', 'review', 'reviewed_at'];
+    protected $fillable = ['amount', 'price', 'sku_id','good_id','order_id','rating', 'review', 'reviewed_at'];
     protected $dates = ['reviewed_at'];
     public $timestamps = false;
 
